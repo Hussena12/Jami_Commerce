1518a4c9b124468e34d9736c2849225ee8481443
Hussen <ha0959943@gmail.com>
2025-08-02 21:26:12 +0300

    metadata setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+export const metadata = {
+  title: "Jami Commerce",
+  icon: "/jami_logo",
+};
 
 export default function Home() {
-  return <div className="font-extrabold">they are</div>;
+  return <div className="font-extrabold text-4xl">we are heavy</div>;
 }
